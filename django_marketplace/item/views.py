327a5fd38fd2e7a9d57a802ff5af2f34e43ccd02
mihstr <micostrnad@gmail.com>
2023-06-19 20:06:13 +0200

    adding edit item option

--- django_marketplace/item/views.py
+++ django_marketplace/item/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import get_object_or_404, render, redirect
 from django.contrib.auth.decorators import login_required
 
-from item.forms import NewItemForm
+from item.forms import EditItemForm, NewItemForm
 from .models import Item
 
 # Create your views here.
@@ -31,6 +31,24 @@ def new(request):
     return render(request, "item/form.html", {
         "form" : form,
         "title" : "New Item",
+    })
+
+@login_required
+def edit(request, pk):
+    item = get_object_or_404(Item, pk=pk, created_by=request.user)
+    if request.method == "POST":
+        form = EditItemForm(request.POST, request.FILES, instance=item)
+
+        if form.is_valid():
+            form.save()
+
+            return redirect("item:detail", pk=item.id)
+    else:
+        form = EditItemForm(instance=item)
+
+    return render(request, "item/form.html", {
+        "form" : form,
+        "title" : "Edit Item",
     })
 
 @login_required

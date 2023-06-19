9a2bdd9c4d5111aea8d488d9304e90e4abdc792b
mihstr <micostrnad@gmail.com>
2023-06-19 20:44:01 +0200

    adding browse function

--- django_marketplace/item/views.py
+++ django_marketplace/item/views.py
@@ -1,8 +1,28 @@
 from django.shortcuts import get_object_or_404, render, redirect
+from django.db.models import Q
 from django.contrib.auth.decorators import login_required
 
 from item.forms import EditItemForm, NewItemForm
-from .models import Item
+from .models import Category, Item
+
+def items(request):
+    query = request.GET.get("query", "")
+    categories = Category.objects.all()
+    category_id = request.GET.get("category", 0)
+    items = Item.objects.filter(is_sold=False)
+
+    if category_id:
+        items = items.filter(category_id=category_id)
+
+    if query:
+        items = items.filter(Q(name__icontains=query) | Q(description__icontains=query))
+
+    return render(request, "item/items.html", {
+        "items": items,
+        "query": query,
+        "categories": categories,
+        "category_id": int(category_id),
+    })
 
 # Create your views here.
 def detail(request, pk):
